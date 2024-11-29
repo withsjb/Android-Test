d1692ab6be31ef45e0779505b0c4dd19a9908dc4
손진빈 <sonjinbin@sonjinbin-ui-MacBookAir.local>
2024-11-29 11:39:07 +0900

    trigertest

--- src/main/java/steps/CommonThenStep.java
+++ src/main/java/steps/CommonThenStep.java
@@ -9,7 +9,7 @@ public class CommonThenStep {
     @Then("11번가 앱 종료")
     public void close11st(){
         String appId = "com.elevenst";
-//        AndroidManager.getDriver().quit();
+//        AndroidManager.getDriver().quit();11
 
 
     }
