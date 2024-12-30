0d5f4d4d27154f606ee056b77b1489252e7726e3
손진빈 <sonjinbin@MacBook-Pro.local>
2024-12-30 16:24:43 +0900

    cucumberreport

--- src/main/java/utils/AndroidManager.java
+++ src/main/java/utils/AndroidManager.java
@@ -1,4 +1,8 @@
 package utils;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.cucumber.java.Scenario;
@@ -11,6 +15,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import steps.Hooks;
+
 
 /**
  비디오 관련 import
@@ -26,8 +31,15 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 // Singleton Design Pattern
 public class AndroidManager {
 
@@ -40,6 +52,7 @@ public class AndroidManager {
 
     private StringBuilder logBuffer = new StringBuilder();
 
+    private static final ObjectMapper objectMapper = new ObjectMapper();
     public AndroidManager(){
         //    private
     }
@@ -215,6 +228,242 @@ public class AndroidManager {
     }
 
 
+
+
+    //cucumber 주간 테스트 결과
+
+    // JSON에서 start_timestamp 값을 추출하여 날짜만 반환
+    public static String getTimestamp(JsonNode jsonNode) {
+        // elements 배열을 가져옴
+        JsonNode elementsNode = jsonNode.get(0).get("elements");
+
+        // elements가 없거나 비어있는 경우 예외 처리
+        if (elementsNode == null || elementsNode.isEmpty()) {
+            throw new RuntimeException("elements 배열이 비어있거나 존재하지 않습니다.");
+        }
+
+        // 첫 번째 시나리오에서 start_timestamp를 추출
+        JsonNode firstElement = elementsNode.get(0);
+        JsonNode timestampNode = firstElement.get("start_timestamp");
+
+        // start_timestamp가 없을 경우 예외 처리
+        if (timestampNode == null || timestampNode.isNull()) {
+            throw new RuntimeException("start_timestamp 필드가 존재하지 않습니다.");
+        }
+
+        // 날짜 부분만 추출하여 반환
+        return timestampNode.asText().split("T")[0];
+    }
+
+
+
+    public static void appendJsonToFile(String sourceFilePath, String destinationFilePath) throws IOException {
+        File sourceFile = new File(sourceFilePath);
+        File destinationFile = new File(destinationFilePath);
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // 기존 파일이 없으면 새로 만듬
+        if (!destinationFile.exists()) {
+            destinationFile.createNewFile();
+        }
+
+        JsonNode sourceJson = readJsonFile(sourceFilePath);
+        JsonNode existingJson = readJsonFile(destinationFilePath);
+
+        // 새로운 JSON 데이터를 기존 데이터 뒤에 추가하는 로직
+        // 이 부분은 기존 JSON 구조에 맞게 데이터를 합치는 방식으로 구현해야 합니다.
+        // 예시로 기존 JSON의 배열에 새 JSON을 추가한다고 가정
+        // 예: existingJson.get("tests").add(sourceJson);
+
+        objectMapper.writerWithDefaultPrettyPrinter().writeValue(destinationFile, existingJson); // 수정된 JSON을 덮어 씌운다.
+    }
+
+    public static JsonNode readJsonFile(String filePath) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readTree(Files.readAllBytes(Paths.get(filePath)));
+    }
+
+    public static void copyJsonFile(String sourceFilePath, String destinationFilePath) throws IOException {
+        File sourceFile = new File(sourceFilePath);
+        File destinationFile = new File(destinationFilePath);
+        if (destinationFile.exists()) {
+            System.out.println("복사 성공");
+            destinationFile.delete(); // 기존 파일 삭제
+        }
+        Files.copy(sourceFile.toPath(), destinationFile.toPath());
+    }
+
+    public static List<Map<String, Object>> readCucumberJson(String filePath) throws IOException {
+        File file = new File(filePath);
+        if (!file.exists()) {
+            return List.of(); // 파일이 없으면 빈 리스트 반환
+        }
+
+        JsonNode rootNode = objectMapper.readTree(file);
+        // rootNode를 원하는 형태로 변환하는 코드 필요
+        return objectMapper.convertValue(rootNode, List.class); // JsonNode를 List로 변환
+    }
+
+    // List<Map<String, Object>> 데이터를 cucumber.json 파일에 저장
+    public static void writeCucumberJson(String filePath, List<Map<String, Object>> data) throws IOException {
+        File file = new File(filePath);
+        // 데이터를 JSON 형식으로 파일에 쓰기
+        objectMapper.writeValue(file, data);
+    }
+//    public static void writeCucumberJson(String filePath, List<Map<String, Object>> results) throws IOException {
+//        ObjectMapper mapper = new ObjectMapper();
+//        FileWriter writer = new FileWriter(filePath);
+//        mapper.writerWithDefaultPrettyPrinter().writeValue(writer, results);
+//    }
+//
+//    public static String getDateFromTimestamp(String timestamp) {
+//        try {
+//            System.out.println("시간 자르기: " + timestamp);  // timestamp 출력 확인
+//            return timestamp.substring(0, 10);  // yyyy-MM-dd 형식으로 잘라서 반환
+//        } catch (Exception e) {
+//            return "시간쪽 에러남";
+//        }
+//    }
+//
+//    public static List<Map<String, Object>> readCucumberJson(String filePath) throws IOException {
+//        ObjectMapper mapper = new ObjectMapper();
+//        File file = new File(filePath);
+//
+//        // 파일이 존재하지 않거나 비어 있으면 빈 리스트 반환
+//        if (!file.exists() || file.length() == 0) {
+//            System.out.println("파일이 없거나 비어 있습니다. 빈 리스트를 반환합니다.");
+//            return new ArrayList<>();
+//        }
+//
+//        // 파일이 존재하면 JSON 데이터를 읽어 리스트로 변환
+//        return mapper.readValue(file, TypeFactory.defaultInstance().constructCollectionType(List.class, Map.class));
+//    }
+//
+//    public static void copyAndUpdateResults(String cucumberJsonPath, String weekJsonPath) throws IOException {
+//        // cucumber.json에서 현재 결과 읽기
+//        List<Map<String, Object>> currentResults = readCucumberJson(cucumberJsonPath);
+//
+//        // cucumberweek.json에서 기존 결과 읽기
+//        List<Map<String, Object>> weekResults = readCucumberJson(weekJsonPath);
+//
+//        // 새로운 결과를 기존 결과에 추가하거나 갱신 (같은 날짜는 갱신)
+//        weekResults = updateResultsWithCurrentDate(weekResults, currentResults);
+//
+//        // cucumberweek.json에 갱신된 결과 저장
+//        writeCucumberJson(weekJsonPath, weekResults);
+//    }
+//
+//
+//    public static List<Map<String, Object>> updateResultsWithCurrentDate(List<Map<String, Object>> weekResults, List<Map<String, Object>> currentResults) {
+//        Map<String, List<Map<String, Object>>> groupedResults = new HashMap<>();
+//
+//        // 기존 weekResults에서 날짜별로 그룹화
+//        for (Map<String, Object> result : weekResults) {
+//            String date = getDateFromTimestamp((String) result.get("start_timestamp"));
+//            groupedResults.putIfAbsent(date, new ArrayList<>());
+//            groupedResults.get(date).add(result);
+//        }
+//
+//        // currentResults에서 날짜별로 그룹화
+//        for (Map<String, Object> result : currentResults) {
+//            String date = getDateFromTimestamp((String) result.get("start_timestamp"));
+//
+//            // 동일 날짜가 있으면 기존 결과를 덮어쓰고, 없으면 새로 추가
+//            if (groupedResults.containsKey(date)) {
+//                List<Map<String, Object>> existingResults = groupedResults.get(date);
+//                // 가장 최근 결과로 덮어쓰는 부분 (필요 시 덮어쓸 추가 로직 구현 가능)
+//                existingResults.clear();
+//                existingResults.add(result);  // 새로운 데이터 추가
+//            } else {
+//                // 해당 날짜가 없으면 새로 추가
+//                groupedResults.put(date, new ArrayList<>(List.of(result)));
+//            }
+//        }
+//
+//        // 결과 리스트로 다시 변환
+//        List<Map<String, Object>> updatedResults = new ArrayList<>();
+//        for (List<Map<String, Object>> dailyResults : groupedResults.values()) {
+//            updatedResults.addAll(dailyResults);
+//        }
+//
+//        return updatedResults;
+//    }
+
+//-----------------------------------******--------------
+
+//    public static List<Map<String, Object>> readCucumberJson(String filePath) throws IOException {
+//        ObjectMapper mapper = new ObjectMapper();
+//        File file = new File(filePath);
+//
+//        if (file.exists()) {
+//            System.out.println("readcucumberjson 함수 234줄 : " + mapper.readValue(file, TypeFactory.defaultInstance().constructCollectionType(List.class, Map.class)));
+//            return mapper.readValue(file, TypeFactory.defaultInstance().constructCollectionType(List.class, Map.class));
+//        } else {
+//            return new ArrayList<>();
+//        }
+//    }
+//
+//    // cucumberweek.json 파일 쓰기
+//    public static void writeCucumberJson(String filePath, List<Map<String, Object>> results) throws IOException {
+//        ObjectMapper mapper = new ObjectMapper();
+//        FileWriter writer = new FileWriter(filePath);
+//        mapper.writerWithDefaultPrettyPrinter().writeValue(writer, results);
+//    }
+//
+//    // ISO 8601 타임스탬프를 밀리초로 변환
+//    public static long parseTimestamp(String timestamp) {
+//        try {
+//            return javax.xml.bind.DatatypeConverter.parseDateTime(timestamp).getTimeInMillis();
+//        } catch (Exception e) {
+//            return 0;
+//        }
+//    }
+//
+//    // ISO 8601 형식에서 날짜만 잘라서 반환 (yyyy-MM-dd)
+//    public static String getDateFromTimestamp(String timestamp) {
+//        try {
+//            System.out.println("시간 자르기: " + timestamp);  // timestamp 출력 확인
+//            return timestamp.substring(0, 10);  // yyyy-MM-dd 형식으로 잘라서 반환
+//        } catch (Exception e) {
+//            return "시간쪽 에러남";
+//        }
+//    }
+//
+//    // 결과를 날짜별로 갱신(덮어쓰기 및 추가)
+//    public static List<Map<String, Object>> updateResultsWithCurrentDate(List<Map<String, Object>> weekResults, List<Map<String, Object>> currentResults) {
+//        Map<String, List<Map<String, Object>>> groupedResults = new HashMap<>();
+//
+//        // weekResults에서 날짜별로 그룹화
+//        for (Map<String, Object> result : weekResults) {
+//            String date = getDateFromTimestamp((String) result.get("start_timestamp"));
+//            groupedResults.putIfAbsent(date, new ArrayList<>());
+//            groupedResults.get(date).add(result);
+//        }
+//
+//        // currentResults에서 날짜별로 그룹화
+//        for (Map<String, Object> result : currentResults) {
+//            String date = getDateFromTimestamp((String) result.get("start_timestamp"));
+//
+//            // 동일 날짜가 있으면 기존 결과를 덮어쓰고, 없으면 새로 추가
+//            if (groupedResults.containsKey(date)) {
+//                // 기존 데이터를 덮어쓰지 않고, 새로운 결과 추가
+//                List<Map<String, Object>> existingResults = groupedResults.get(date);
+////                existingResults.clear();  // 기존 결과 삭제
+//                existingResults.add(result);  // 새로운 결과 추가
+//            } else {
+//                // 해당 날짜가 없으면 새로 추가
+//                groupedResults.put(date, new ArrayList<>(List.of(result)));
+//            }
+//        }
+//
+//        // 결과 리스트로 다시 변환
+//        List<Map<String, Object>> updatedResults = new ArrayList<>();
+//        for (List<Map<String, Object>> dailyResults : groupedResults.values()) {
+//            updatedResults.addAll(dailyResults);  // 모든 날짜의 결과를 다시 리스트로 합침
+//        }
+//
+//        return updatedResults;
+//    }
 }
 
 
--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -1,10 +1,18 @@
 package runner;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+import utils.AndroidManager;
 import utils.XrayReportUploader;
+
+import java.util.List;
+import java.util.Map;
+
+import static utils.AndroidManager.copyJsonFile;
+import static utils.AndroidManager.readJsonFile;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/main/assets/features/", glue = "steps", plugin = {
@@ -12,10 +20,58 @@ import utils.XrayReportUploader;
         "json:target/cucumber.json"          // JSON 형식으로 결과 출력
 })
 public class Runner {
+
+    private static final String CUCUMBER_JSON = "target/cucumber.json";
+    private static final String CUCUMBER_WEEK_JSON = "target/cucumberweek.json";
+
+
     @AfterClass
     public static void uploadReport(){
         try{
+
             XrayReportUploader.XrayReport(new String[]{});
+
+            while (!new java.io.File(CUCUMBER_JSON).exists()) {
+                Thread.sleep(100); // 100ms 간격으로 체크
+            }
+
+            JsonNode newJson = readJsonFile(CUCUMBER_JSON);
+            String newTimestamp = AndroidManager.getTimestamp(newJson);  // getTimestamp 메서드로 처리
+
+            // cucumberweek.json 파일을 읽고 비교
+            JsonNode existingJson = readJsonFile(CUCUMBER_WEEK_JSON);
+            String existingTimestamp = AndroidManager.getTimestamp(existingJson);  // "2024-12-30"
+
+            if (existingJson != null) {
+                // 결과를 병합하는 로직
+                List<Map<String, Object>> currentResults = AndroidManager.readCucumberJson(CUCUMBER_JSON);
+                List<Map<String, Object>> weekResults = AndroidManager.readCucumberJson(CUCUMBER_WEEK_JSON);
+
+                // 새로운 결과를 기존 cucumberweek.json에 추가
+                weekResults.addAll(currentResults);
+
+                // cucumberweek.json에 병합된 결과 저장
+                AndroidManager.writeCucumberJson(CUCUMBER_WEEK_JSON, weekResults);
+            } else {
+                // cucumberweek.json이 존재하지 않으면 새로운 파일을 생성하고 결과를 기록
+                List<Map<String, Object>> currentResults = AndroidManager.readCucumberJson(CUCUMBER_JSON);
+                AndroidManager.writeCucumberJson(CUCUMBER_WEEK_JSON, currentResults);
+            }
+            // cucumber.json 읽기
+//            List<Map<String, Object>> currentResults = AndroidManager.readCucumberJson(CUCUMBER_JSON);
+//
+//            // cucumberweek.json 파일 읽기
+//            List<Map<String, Object>> weekResults = AndroidManager.readCucumberJson(CUCUMBER_WEEK_JSON);
+//
+//            // 새로운 결과를 주어진 파일에 추가(같은 날짜면 갱신)
+//            weekResults = AndroidManager.updateResultsWithCurrentDate(weekResults, currentResults);
+//
+//            // 결과 출력하여 확인
+//            System.out.println("Updated weekResults: " + weekResults);
+//
+//            // cucumberweek.json에 결과 저장
+//            AndroidManager.writeCucumberJson(CUCUMBER_WEEK_JSON, weekResults);
+//            AndroidManager.copyAndUpdateResults(CUCUMBER_JSON, CUCUMBER_WEEK_JSON);
 
         } catch (Exception e) {
             throw new RuntimeException(e);
